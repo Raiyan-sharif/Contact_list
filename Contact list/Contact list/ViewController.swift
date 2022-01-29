60947cffe8f696374b0deb246ce5509c641f94e7
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2022-01-30 02:28:22 +0600

    project initilization

---
+++ Contact list/Contact list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact list
+//
+//  Created by raiyan sharif on 30/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
